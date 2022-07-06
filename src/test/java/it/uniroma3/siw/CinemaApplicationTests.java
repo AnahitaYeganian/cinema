58b0d4541062357c274a3fb17cfd8b27dad8bf73
Anahita Yeganian <anahitayeganian@gmail.com>
2022-07-06 18:22:39 +0200

    First commit. Cinema project created.

---
+++ src/test/java/it/uniroma3/siw/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
