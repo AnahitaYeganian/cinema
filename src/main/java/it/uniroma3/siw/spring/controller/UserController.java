e2e5b58f397c32b8ddc54b3106a6133b8bf38333
Anahita Yeganian <anahitayeganian@gmail.com>
2022-07-11 14:37:21 +0200

    Update user details operation implemented.

--- src/main/java/it/uniroma3/siw/spring/controller/UserController.java
+++ src/main/java/it/uniroma3/siw/spring/controller/UserController.java
@@ -3,12 +3,20 @@ package it.uniroma3.siw.spring.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import it.uniroma3.siw.spring.controller.validator.CredentialsValidator;
+import it.uniroma3.siw.spring.controller.validator.UserValidator;
+import it.uniroma3.siw.spring.model.Credentials;
 import it.uniroma3.siw.spring.model.MovieReservation;
 import it.uniroma3.siw.spring.model.User;
 import it.uniroma3.siw.spring.service.UserService;
@@ -19,6 +27,12 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private UserValidator userValidator;
+	
+	@Autowired
+	private CredentialsValidator credentialsValidator;
+	
 	@GetMapping("/home/reservation")
 	public String getAllReservationPerUser(Model model, HttpSession session) {
 		User currentUser = (User)session.getAttribute("currentUser");
@@ -27,5 +41,37 @@ public class UserController {
 		model.addAttribute("reservations", reservations);
 		return "myReservations.html";
 	}
+	
+	@GetMapping("/updateUser/{id}")
+	public String updateUser(@PathVariable("id") Long userId, Model model) {
+		model.addAttribute("user", this.userService.getUser(userId));
+		model.addAttribute("credentials", this.userService.getCredentialsService().findCredentialsByUser_Id(userId));
+		return "userDetailsForm.html";
+	}
+	
+	@PostMapping("/updateUser")
+    public String updateUser(@Valid @ModelAttribute("user") User user, BindingResult userBindingResult,
+                 @Valid @ModelAttribute("credentials") Credentials credentials, BindingResult credentialsBindingResult, Model model, HttpSession session) {
+		
+        this.userValidator.validate(user, userBindingResult);
+        this.credentialsValidator.validate(credentials, credentialsBindingResult);
+
+        if(!userBindingResult.hasErrors() && ! credentialsBindingResult.hasErrors()) {
+        	User currentUser = (User)session.getAttribute("currentUser");
+        	Credentials currentCredentials = (Credentials)session.getAttribute("currentCredentials");
+        	
+        	currentUser.setName(user.getName());
+        	currentUser.setSurname(user.getSurname());
+        	currentCredentials.setUsername(credentials.getUsername());
+        	currentCredentials.setPassword(credentials.getPassword());
+        	
+        	//currentCredentials.setUser(currentUser); non serve
+            this.userService.getCredentialsService().saveCredentials(currentCredentials);
+            //model.addAttribute("modificaAvvenuta", new String("L'utente inserito è stato registrato"));
+            return "home.html";
+        }
+        
+        return "userDetailsForm.html";
+    }
 
 }
